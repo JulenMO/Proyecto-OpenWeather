4f27894a34bea8556308b6d586e08212ca3ff86c
julenmo <julen.medina.oses@gmail.com>
2024-11-08 22:31:24 +0100

    Funcionalidades básicas de algunos botones

--- funcionalidad/index.js
+++ funcionalidad/index.js
@@ -1,4 +1,22 @@
 jQuery(document).ready(function()
 {
-    
+    var seccionHome = $('#seccionHome');
+    var seccionBuscar = $('#seccionBuscar');
+    var resultadoWeather = $('#resultadoWeather');
+    seccionBuscar.hide();
+    resultadoWeather.hide();
+    $('#btnDesplegarColumna').on('click', function () {
+        $('#columnaDesplegable').toggleClass('d-none');
+    });
+    $('#irHome').on('click', function () {
+        seccionHome.show();
+        seccionBuscar.hide();
+        resultadoWeather.hide();
+    });
+    $('#irBuscar').on('click', function () {
+        
+    });
+    $('#irLocalizacion').on('click', function () {
+        
+    });
 });
